51739304829faa9c0440a060d7d2242d069d704d
Julian alvarez <alvarezjulianignacio@gmail.com>
2020-09-09 19:58:26 -0300

    removed healthCheck

--- app/controllers/healthCheck.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import HttpStatus from 'http-status-codes';
-
-export function healthCheck(_: Request, res: Response): Response {
-  return res.status(HttpStatus.OK).send({ uptime: process.uptime() });
-}
--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,8 @@
 import { Application } from 'express';
 
-import { healthCheck } from './controllers/healthCheck';
 import { getProducts, getProductById } from './controllers/products';
 
 export const init = (app: Application): void => {
-  app.get('/health', healthCheck);
   app.get('/products', getProducts);
   app.get('/products/:id', getProductById);
 };
